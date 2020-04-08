1a592d30030a6deae9f58ae7d46987568ca49c24
alips96 <alips96@gmail.com>
2020-04-08 12:27:33 +0200

    adjusting difficulty level

--- Assets/Scripts/ColumnSpawner.cs
+++ Assets/Scripts/ColumnSpawner.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class ColumnSpawner : MonoBehaviour
 {
@@ -6,6 +7,7 @@ public class ColumnSpawner : MonoBehaviour
     private float yPos;
     private float nextCheck;
     private float checkRate = 2;
+    private float deductionRate;
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +25,34 @@ public class ColumnSpawner : MonoBehaviour
     {
         if(Time.time > nextCheck)
         {
-            Vector3 spawnPos = new Vector3(transform.position.x, Random.Range(yPos - 1, yPos + 2.8f));
+            Vector3 spawnPos = new Vector3(transform.position.x, UnityEngine.Random.Range(yPos - 1, yPos + 2.8f));
             GameObject column = Instantiate(columnPrefab, spawnPos, Quaternion.identity);
+            SetColumnSize(column); // To adjust difficulty level.
             Destroy(column, 3.75f);
             nextCheck = Time.time + checkRate;
         }
 
+    }
+
+    private void SetColumnSize(GameObject column)
+    {
+        Transform[] transforms = column.GetComponentsInChildren<Transform>();
+
+        foreach (Transform item in transforms)
+        {
+            if (item.position.y > 0)
+            {
+                item.position = new Vector3(item.position.x, item.position.y - deductionRate, item.position.z);
+            }
+            else
+            {
+                item.position = new Vector3(item.position.x, item.position.y + deductionRate, item.position.z);
+            }
+        }
+
+        if (Mathf.Abs(transforms[0].position.y - transforms[1].position.y) > 3.9f) // stop the game from getting to hard.
+        {
+            deductionRate += .02f;
+        }
     }
 }
