c1b7102dba7074e40e43f4959541695525cdccdf
Ali Poorsolaymani <alips96@gmail.com>
2024-11-26 18:13:35 +0100

    Load Gameplay Scene
    When all players have joined the server, load the gameplay scene.

--- Assets/Flappy Bird/Scripts/Main Menu/MainMenuManager.cs
+++ Assets/Flappy Bird/Scripts/Main Menu/MainMenuManager.cs
@@ -2,6 +2,7 @@ using System;
 using TMPro;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
@@ -10,6 +11,25 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private Text statusText;
 
     private const string EMPTY_NAME_MESSAGE = "Your name is empty! Try again.";
+    private const int MAX_PLAYERS_COUNT = 2;
+
+    private void Start()
+    {
+        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+    }
+
+    private void OnClientConnected(ulong clientId)
+    {
+        Debug.Log($"Client with ID {clientId} has connected to the server.");
+
+        if (NetworkManager.Singleton.IsServer)
+        {
+            if (NetworkManager.Singleton.ConnectedClients.Count == MAX_PLAYERS_COUNT)
+            {
+                NetworkManager.Singleton.SceneManager.LoadScene("TestScene", LoadSceneMode.Single);
+            }
+        }
+    }
 
     public void OnStartClient(bool connectAsHost) //Called by UI buttons.
     {
